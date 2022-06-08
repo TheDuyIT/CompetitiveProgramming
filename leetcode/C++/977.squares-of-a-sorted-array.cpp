e4450d3e0d09d709165c4d2c8ec0710abd72fde1
theduy <duyng@vingroup.net>
2022-06-08 17:40:31 +0700

    shorter version 997 leetcode

--- leetcode/C++/977.squares-of-a-sorted-array.cpp
+++ leetcode/C++/977.squares-of-a-sorted-array.cpp
@@ -8,61 +8,21 @@ class Solution
 public:
     vector<int> sortedSquares(vector<int> &nums)
     {
-        int curr_idx = 0;
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            curr_idx = i;
-            if (nums[i] >= 0)
-                break;
-        }
-        int point_1 = curr_idx - 1;
-        int point_2 = curr_idx;
+        int curr_idx = nums.size() - 1, left = 0, right = nums.size() - 1;
         vector<int> rest(nums.size());
-        curr_idx = 0;
-        for (int i = point_1, j = point_2; i >= 0 || j < nums.size();)
+
+        while (left <= right)
         {
-            
-            if (i == j)
-            {
-                rest[curr_idx] = nums[i] *nums[i];
-                --i;
-                ++j;
-                ++curr_idx;
-                continue;
+            int left2 = nums[left] * nums[left];
+            int right2 = nums[right] * nums[right];
+            if (left2 < right2){
+                rest[curr_idx--] = right2;
+                --right;
             }
-            if (i < 0){
-                rest[curr_idx] = nums[j] * nums[j];
-                j++;
-                ++curr_idx;
-                continue;
+            else{
+                rest[curr_idx--] = left2;
+                ++left;
             }
-            if (j >= nums.size()){
-                rest[curr_idx] = nums[i]  * nums[i];
-                --i;
-                ++curr_idx;
-                continue;
-            }
-            int i2 = nums[i] * nums[i], j2 = nums[j] * nums[j];
-            if (i2 < j2)
-            {
-                rest[curr_idx] = i2;
-                i--;
-            }
-            else if (i2 > j2)
-            {
-                rest[curr_idx] = j2;
-                ++j;
-            }
-            else
-            {
-                rest[curr_idx] = j2;
-                ++curr_idx;
-                rest[curr_idx] = j2;
-                
-                --i;
-                ++j;
-            }
-            ++curr_idx;
         }
         return rest;
     }
@@ -71,9 +31,9 @@ public:
 int main()
 {
     Solution sol = Solution();
-    // vector<int> nums{-7, -3, 2, 3, 11};
+    vector<int> nums{-7, -3, 2, 3, 11};
     // vector<int> nums{-7, -3, -2, -1};
-    vector<int> nums{-1, 2, 2};
+    // vector<int> nums{-1, 2, 2};
     auto rest = sol.sortedSquares(nums);
     for (auto num : rest)
     {
